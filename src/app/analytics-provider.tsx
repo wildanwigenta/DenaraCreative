651c13b1a63626265f02637738a0668646f791a1
Wildan <wildanwigenta52@gmail.com>
2025-10-05 15:24:32 +0700

    fix search param

--- src/app/analytics-provider.tsx
+++ src/app/analytics-provider.tsx
@@ -1,17 +1,27 @@
 'use client'
 
+import { Suspense, useEffect } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
 import * as gtag from '@/lib/gtag'
 
-export default function AnalyticsProvider() {
+function AnalyticsTracker() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
   useEffect(() => {
-    const url = pathname + searchParams.toString()
+    // Pastikan URL terbentuk benar
+    const url = pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '')
     gtag.pageview(url)
   }, [pathname, searchParams])
 
   return null
 }
+
+export default function AnalyticsProvider() {
+  return (
+    <Suspense fallback={null}>
+      <AnalyticsTracker />
+    </Suspense>
+  )
+}
+
