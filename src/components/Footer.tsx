e8f2e016f620ca396f66c701bb729bd290156128
Wildan <wildanwigenta52@gmail.com>
2025-11-25 20:54:41 +0700

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     { name: 'Home', href: '/' },
     { name: 'About Us', href: '/about' },
     { name: 'Services', href: '/services' },
-    { name: 'Portfolio', href: '/portfolio' },
+    { name: 'Portofolio', href: '/portofolio' },
   ];
 
   const services = [
